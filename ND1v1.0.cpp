338d7d9b3f3d5894aede6fce8e90e86a8f2a1054
Aidas Bulakas <aidas.bulakas@mif.stud.vu.lt>
2020-02-12 21:26:54 +0200

    Pataisyta pilna 1.0 versija

--- ND1v1.0.cpp
+++ ND1v1.0.cpp
@@ -4,44 +4,79 @@
 
 using namespace std;
 
+struct sveikinimas {
+    string krastas="", vidurys="", uzpildas="";
+};
+
 int main()
 {
-    string vardas, treciaE;
+    string vardas;
     char galune;
-    int charSkc, nPlotis;
-    string remelis="", pasisveikinimas;
+    sveikinimas Remelis;
+    int eilIlgis, nPlotis, vidurioEilute;
 
     cout<<"Iveskite savo varda: ";
     cin>>vardas;
 
-    charSkc = vardas.length() + treciaE.length() + 3;
+    // tikrinama, kokios gimines yra ivestas vardas
+    galune=vardas[vardas.length()-1];
+    if (galune=='a' || galune=='e' || galune=='ė' || galune=='A' || galune=='E' || galune=='Ė') {
+        Remelis.vidurys="* Sveika, "+vardas+"! *"; // sukuriama vidurine eilute, mot. gimine
+    }
+    else {
+        Remelis.vidurys="* Sveikas, "+vardas+"! *"; // sukuriama vidurine eilute, vyr. gimine
+    }
+
+   // charSkc = vardas.length() + Remelis.vidurys.length() + 3;
 
     bool ok = false;
     // tikrinama ar ivestas plotis ne per mazas pasisveikinimui
     do {
-        cout<<"Iveskite kokio plocio remelio noretumete (minimalus galimas plotis = " << charSkc << "): ";
+        cout<<"Iveskite kokio aukscio remelio noretumete (minimalus galimas eiluciu kiekis - aukstis = 3):";
         cin>>nPlotis;
 
-        if(nPlotis<charSkc){
-            cout<<"\n naujai ivestas plotis turi buti ne mazesnis uz "<<charSkc<<"!\n";
+        if(nPlotis<3){
+            cout<<"\nnaujai ivestas plotis turi buti ne mazesnis uz 3!\n";
         }
-        else ok = true;
+        else {
+            ok = true;
+        }
     } while (!ok);
 
-    // tikrinama, kokios gimines yra ivestas vardas
-    galune=vardas[vardas.length()-1];
-    if (galune=='a' || galune=='e' || galune=='ė') {
-        pasisveikinimas="Sveika, "+vardas+"!";
+    if (nPlotis%2==0){
+        vidurioEilute=nPlotis/2;
     }
     else {
-        pasisveikinimas="Sveikas, "+vardas+"!";
+        vidurioEilute=nPlotis/2+1;
+    }
+    eilIlgis = Remelis.vidurys.length(); cout<<eilIlgis<<endl;
+    Remelis.krastas.insert(Remelis.krastas.begin(), eilIlgis, '*'); // padaroma virsaus ir apacios eilute
+
+    // padaroma kitos, nei vidurio, virsaus ar apacios eilutes
+    Remelis.uzpildas+='*';
+    Remelis.uzpildas.insert(Remelis.uzpildas.begin()+1, eilIlgis-2, ' ');
+    Remelis.uzpildas+='*';
+
+    for (int i=1; i<=nPlotis; i++){
+        if (i==1 || i==nPlotis){
+            cout<<Remelis.krastas<<endl;
+        }
+        else if (i==vidurioEilute){
+            cout<<Remelis.vidurys<<endl;
+        }
+        else {
+              cout<<Remelis.uzpildas<<endl;
+        }
     }
 
+
+
+    /*
     // sukuriamas pasisveikinimas, busiantis trecioje eiluteje
     treciaE+="*";
     treciaE.insert(treciaE.begin()+1, ((nPlotis-2)/2)-pasisveikinimas.length()/2, ' ');
     treciaE+=pasisveikinimas;
-    treciaE.insert(treciaE.begin()+treciaE.length(), ((nPlotis-3)/2)-pasisveikinimas.length()/2, ' ');
+    treciaE.insert(treciaE.begin()+treciaE.length(), ((nPlotis-3)/2)-(pasisveikinimas.length()/2), ' ');
     treciaE+="*";
 
     // sukuriamas remelis su pasisveikinimu
@@ -64,7 +99,7 @@ int main()
 
     // remelis spausdinamas
     cout<<remelis<<endl;
-
+    */
     system("pause");
 
     return 0;
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-#Pasisveikinimas rėmelyje
+# Pasisveikinimas rėmelyje
 Asmeninis pasisveikinimas rėmelyje - versija 1.0
 
 1. Programos funkcija:
@@ -7,6 +7,6 @@ Asmeninis pasisveikinimas rėmelyje - versija 1.0
 2. Programos release'ai:
 	1. v0.1 - pasisveikinimas 'Sveikas, ' + įvestas vardas.  '\*' simbolių rėmelis.
 	2. v0.2 - pasisveikinimas kinta (Sveikas/Sveika), priklausomai nuo įvesto vardo giminės.
-	3. v1.0 - galima reguliuoti rėmelio plotį. Kiekviena eilutė nėra įrašyta atskirame kintamajame.
+	3. v1.0 - galima reguliuoti rėmelio aukštį. Kiekviena eilutė nėra įrašyta atskirame kintamajame.
 3. Įdiegimo instrukcija:
 	Tiesiog pasileiskite darbas1v1.0.exe failą ir vykdykite ekrane matomas instrukcijas. Papildomai instaliuoti nieko nereikia.
